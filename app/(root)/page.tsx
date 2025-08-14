878e7b6a32d3f21d44304c7ae2bf6952eaa08755
spywolf2049 <rishabhbaruah87@gmail.com>
2025-08-14 19:32:55 +0530

    updatew

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <div>
+      <div className='uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[3.375rem] sm:leading-[4rem] text-[2.25rem] leading-[2.875rem] max-w-5xl text-center my-5' >
         Home
       </div>
     </>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation"; // Fixed import
 const Navbar = async () => {
   const session = await auth();
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <header className="px-5 py-4 bg-white  font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
