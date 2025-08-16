b3adfb745832bc35bb51e21ce5e49cd53eb7ce00
spywolf2049 <rishabhbaruah87@gmail.com>
2025-08-16 14:35:01 +0530

    added

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -2,6 +2,9 @@ import type {StructureResolver} from 'sanity/structure'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
-  S.list()
+S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      S.documentTypeListItem('author' ).title('Author'),
+      S.documentTypeListItem('startup' ).title('Startup')
+    ]); 
