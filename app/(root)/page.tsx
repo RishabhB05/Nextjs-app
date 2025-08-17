cf883784b1c9a4f1928e17fc8b1662308974e633
spywolf2049 <rishabhbaruah87@gmail.com>
2025-08-17 18:04:41 +0530

    c

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,18 +2,29 @@ import { Search } from 'lucide-react';
 import React from 'react';
 import SearchForm from '../../components/SearchForm';
 import { title } from 'process';
-import StartupCard from '@/components/StartupCard';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 import { client } from '@/sanity/lib/client';
 
 const Home = async ({ searchParams }: { searchParams: { query?: string } }) => {
     const query = (await searchParams).query;
+   const params = { search: query || null };
     const posts = await client.fetch(STARTUPS_QUERY, { search: query });
-      
     
+      // const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+
   return (
     <>
       <section className="w-full min-h-[530px] bg-yellow-500 bg-[repeating-linear-gradient(to_right,white,white_0px,transparent_1px,transparent_60px)] flex justify-center items-center flex-col py-10 px-6">
+        
+        <div className='bg-[#EE2B69] p-3 rounded-md '>
+            <div className="font-work-sans font-bold uppercase text-[17px] leading-[24px] tracking-[0.06em] w-[231px] h-[24px] flex items-center">
+              PITCH, VOTE, AND GROW
+            </div>
+          </div>
+
+
         <div className="uppercase bg-black px-6 py-3 font-extrabold text-white sm:text-[3.375rem] sm:leading-[4rem] text-[2.25rem] leading-[2.875rem] max-w-5xl text-center my-5">
           Pitch Your Startup <br /> Connect with Entrepreneurs
         </div>
@@ -31,28 +42,15 @@ const Home = async ({ searchParams }: { searchParams: { query?: string } }) => {
          {query ? `Search results for: ${query}` : "All Startups"}
        </p>
 
-    <ul className=' mt-7 grid md:grid-cols-3 sm:grid-cols-2 gap-5'>
-          {posts.length > 0 ? (
-            // posts.map((post) => (
-            //   <li key={post.title} className='bg-white rounded-lg shadow-md p-5'>
-            //     <img src={post.image} alt={post.title} className='w-full h-40 object-cover rounded-md' />
-            //     <h3 className='text-lg font-bold mt-2'>{post.title}</h3>
-            //     <p className='text-sm text-gray-600'>{post.description}</p>
-            //     <div className='mt-2'>
-            //       <span className='text-xs text-gray-500'>{post.category}</span>
-            //     </div>
-            //   </li>
-            // ))
-            
-            posts.map((post: any, index: number )=>(
-              <StartupCard key={post?.id || post?.title || index} post = {post} />
+     <ul className="mt-7     grid md:grid-cols-3 sm:grid-cols-2 gap-5      ">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
             ))
-
           ) : (
-            <li className='col-span-3 text-center'>No posts found.</li>
+            <p className="no-results">No startups found</p>
           )}
-    </ul>
-
+        </ul> 
 
     </section>
 
