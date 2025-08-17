64d2f8a6014a57d068cb830e81335a381d4a502a
spywolf2049 <rishabhbaruah87@gmail.com>
2025-08-17 18:42:51 +0530

    live

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -2,19 +2,19 @@
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
 
-import { defineLive } from "next-sanity";
-import { client } from './client'
-
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    apiVersion: 'vX' 
-  }) 
-});
-
-
-// import "server-only";
-
 // import { defineLive } from "next-sanity";
-// import { client } from "@/sanity/lib/client";
+// import { client } from './client'
 
-// export const { sanityFetch, SanityLive } = defineLive({ client });
+// export const { sanityFetch, SanityLive } = defineLive({ 
+//   client: client.withConfig({ 
+//     apiVersion: 'vX' 
+//   }) 
+// });
+
+
+import "server-only";
+
+import { defineLive } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+
+export const { sanityFetch, SanityLive } = defineLive({ client });
