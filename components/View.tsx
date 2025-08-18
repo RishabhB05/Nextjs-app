46096b7c851006ad48cad1940bdf892d66ec79e4
spywolf2049 <rishabhbaruah87@gmail.com>
2025-08-18 11:05:32 +0530

    commit

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import Ping from './ping';
+import { client } from '@/sanity/lib/client';
+import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
+import { after } from 'next/server';
 
 interface ViewProps {
   id: string;
 }
 
-const View = ({ id }: {id:string}) => {
+const View = async ({ id }: {id:string}) => {
+
+   const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  // after(
+  //   async () =>
+  //     await writeClient
+  //       .patch(id)
+  //       .set({ views: totalViews + 1 })
+  //       .commit(),
+  // );
+
+
+
   return (
     <div className="flex justify-end items-center mt-5 fixed bottom-3 right-3">
          <div className='absolute -top-2 -right-2'>
@@ -13,7 +31,7 @@ const View = ({ id }: {id:string}) => {
          </div>
     
     <p className='font-medium text-[16px] bg-pink-200 px-4 py-2 rounded-lg capitalize'>
-           <span className='font-black'>100 views</span>
+           <span className='font-black'>Views: {totalViews}</span>
     </p>
 
     </div>
